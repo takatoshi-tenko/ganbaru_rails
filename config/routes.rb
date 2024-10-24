3eb4778e0f87ab04f60f400b3d21804892531cb6
takatoshi.tenko <chanten811@gmail.com>
2024-10-25 00:17:33 +0900

    create: article controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  get "/articles", to: "articles#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
