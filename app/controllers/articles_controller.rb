b3aee83db470594a0571c3b180d574d455afe73a
takatoshi.tenko <chanten811@gmail.com>
2024-10-25 09:07:30 +0900

    add: new, create method, new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "Hello, World!", body: "This is the body of the article.")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
