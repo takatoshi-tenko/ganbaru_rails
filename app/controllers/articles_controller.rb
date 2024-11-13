bd570f649f1164349d4d6ed2bd9bea32adc962a0
takatoshi.tenko <chanten811@gmail.com>
2024-11-13 22:18:07 +0900

    newページの修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "Hello, World!", body: "This is the body of the article.")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def article_params
+    # params.expect(article: [ :title, :body ])
+    params.require(:article).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  get "/articles", to: "articles#index"
+  resources :articles
   get "/articles/:id", to: "articles#show"
 
   # Defines the root path route ("/")
